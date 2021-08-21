d4ba2e63f07511e5ccb694a3f39ea4b6d016887d
Pavel Li <paul.lee.ev@gmail.com>
2021-08-21 20:32:41 +0400

    Add ViewModel

--- app/src/main/java/org/devlee/example/mvvm/notes/ui/main/MainViewModel.kt
+++ app/src/main/java/org/devlee/example/mvvm/notes/ui/main/MainViewModel.kt
@@ -1,7 +1,38 @@
 package org.devlee.example.mvvm.notes.ui.main
 
+import android.text.format.DateFormat
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.launch
+import org.devlee.example.mvvm.notes.locateLazy
+import org.devlee.example.mvvm.notes.repository.Repository
+import org.devlee.example.mvvm.notes.repository.room.Note
+import java.util.*
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val repository: Repository by locateLazy()
+
+    val notes = repository.getAll().asLiveDataFlow()
+
+    fun save(note: String) {
+        viewModelScope.launch { repository.save(createNote(note)) }
+    }
+
+    fun delete(note: Note) {
+        viewModelScope.launch { repository.delete(note) }
+    }
+
+    private fun createNote(noteText: String) = Note(
+        caption = createCaption(),
+        text = noteText
+    )
+
+    private fun createCaption(): String =
+        DateFormat.format("hh:mm:ss, MMM dd, yyyy", Date()).toString()
+
+    fun <T> Flow<T>.asLiveDataFlow() = shareIn(viewModelScope, SharingStarted.Eagerly, replay = 1)
 }
