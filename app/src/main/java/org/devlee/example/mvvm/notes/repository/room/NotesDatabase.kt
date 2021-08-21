68a9b2953f6379a390dbad2bd48c3563f6c5fc1b
Pavel Li <paul.lee.ev@gmail.com>
2021-08-21 20:26:01 +0400

    Add Room

--- app/src/main/java/org/devlee/example/mvvm/notes/repository/room/NotesDatabase.kt
+++ app/src/main/java/org/devlee/example/mvvm/notes/repository/room/NotesDatabase.kt
@@ -6,14 +6,16 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Note::class], version = 1)
-abstract class NotesDatabase: RoomDatabase() {
+abstract class NotesDatabase : RoomDatabase() {
     abstract val notesDao: NotesDao
 
     companion object {
-        fun create(context: Context) = Room.databaseBuilder(
-            context,
-            NotesDatabase::class.java,
-            "notes-database"
-        )
+        fun create(context: Context) = Room
+            .databaseBuilder(
+                context,
+                NotesDatabase::class.java,
+                "notes-database"
+            )
+            .build()
     }
 }
